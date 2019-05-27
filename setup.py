feaf6ac47059505de19bc4c20a4b44c390264cac
Basti Tee <basti.tee@posteo.de>
2019-05-27 15:25:55 +0200

    Fix dev version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(path.abspath(path.dirname(__file__)), 'README.md'),
 setup(
     # Basic project information
     name='pype-cli',
-    version='0.0.6',
+    version='0.0.7-SNAPSHOT',
     # Authorship and online reference
     author='Basti Tee',
     author_email='basti.tee@posteo.de',
