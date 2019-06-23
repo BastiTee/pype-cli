39dd0f7ac3f1845b420a42e459bcbff4bf2abbde
Basti Tee <basti.tee@posteo.de>
2019-06-23 21:48:57 +0200

    Add dynamic subcommand routine

--- pype/util/cli.py
+++ pype/util/cli.py
@@ -3,6 +3,46 @@
 
 import click
 
+from pype.util import misc
+
+
+def _append_short_names_to_commands(commands):
+    stripped_keys = misc.remove_common_prefix_from_string_list(
+        misc.remove_common_suffix_from_string_list(
+            [command['name'] for command in commands]
+        ))
+    for i in range(0, len(commands)):
+        commands[i]['short_name'] = stripped_keys[i]
+
+
+def _get_command(name, commands, strip):
+    selector = 'short_name' if strip else 'name'
+    for command in commands:
+        if command[selector] == name:
+            return command
+
+
+def generate_dynamic_multicommand(
+        commands, callback_function, strip=False):
+    """Use provided dict to create a dynamic click.MultiCommand."""
+    if strip:
+        _append_short_names_to_commands(commands)
+
+    class DynamicCLI(click.MultiCommand):
+
+        def list_commands(self, ctx):
+            selector = 'short_name' if strip else 'name'
+            return [command[selector] for command in commands]
+
+        def get_command(self, ctx, name):
+            command = _get_command(name, commands, strip)
+            @click.command(name, help=command.get('help', None))
+            @click.pass_context
+            def invoke_callback(ctx):
+                callback_function(command['name'], ctx)
+            return invoke_callback
+    return DynamicCLI
+
 
 def ask_yes_or_no(question, default='yes'):
     """Ask user a yes/no question and return their answer."""
