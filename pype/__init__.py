777fab9fb25dea1cb8f1fa442614de750dfd50f0
Sebastian Tschöpel <sebastian.tschoepel@leanix.net>
2019-11-01 16:44:56 +0100

    Apply new pype-cli facade

--- pype/__init__.py
+++ pype/__init__.py
@@ -5,6 +5,7 @@
 :license: Apache 2.0, see LICENSE for more details.
 """
 
+from pype.core import print_context_help  # noqa: F401
 from pype.util.cli import fname_to_name  # noqa: F401
 from pype.util.cli import print_error  # noqa: F401
 from pype.util.cli import print_success  # noqa: F401
