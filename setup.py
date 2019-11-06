06509c306fab33dd041ecb89f61a6af1e856d581
Basti Tee <basti.tee@posteo.de>
2019-11-06 22:25:31 +0100

    Revert support for python 3.4

--- setup.py
+++ setup.py
@@ -36,7 +36,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
@@ -45,7 +44,7 @@ setup(
     # Package configuration
     packages=find_packages(exclude=('tests',)),
     include_package_data=True,
-    python_requires='>=3.4',
+    python_requires='>=3.5',
     install_requires=[
         'click',
         'tabulate',
