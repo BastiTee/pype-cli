fd94d5422a10b846947219a61a3702537d904ede
Basti Tee <basti.tee@posteo.de>
2019-11-07 09:03:56 +0100

    Add short aliases for run_interactive and run_and_get_output

--- pype/__init__.py
+++ pype/__init__.py
@@ -13,5 +13,7 @@ from pype.util.cli import print_warning  # noqa: F401
 from pype.util.iotools import open_with_default  # noqa: F401
 from pype.util.iotools import resolve_path  # noqa: F401
 from pype.util.iotools import run_and_get_output  # noqa: F401
+from pype.util.iotools import run_and_get_output as sho  # noqa: F401
 from pype.util.iotools import run_interactive  # noqa: F401
+from pype.util.iotools import run_interactive as sh  # noqa: F401
 from pype.util.multi_cmd import generate_dynamic_multicommand  # noqa: F401
