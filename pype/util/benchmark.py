4eba2b7d1b0efcb64e30c27f78e43ef334e7f9cc
Sebastian Tschöpel <sebastian.tschoepel@leanix.net>
2020-08-19 15:13:50 +0200

    Improve benchmarking

--- pype/util/benchmark.py
+++ pype/util/benchmark.py
@@ -1,31 +1,35 @@
-"""Benchmark."""
+# -*- coding: utf-8 -*-
+"""Internal helper to benchmark initialization process."""
 
 from os import environ
+from sys import stderr
 from time import time
 
 ENV_BENCHMARK_INIT = 'PYPE_BENCHMARK_INIT'
 
 
 class Benchmark():
-    def __init__(self, key=None):
+    """Benchmark context."""
+
+    def __init__(self, key):  # noqa: D107
         self.active = self.__is_active()
         if not self.active:
             return
         self.key = key
         pass
 
-    def __enter__(self):
+    def __enter__(self):  # noqa: D105
         if not self.active:
             return
         self.start_time = time()
 
-    def __exit__(self, type_s, value, traceback):
+    def __exit__(self, type_s, value, traceback):  # noqa: D105
         if not self.active:
             return
         self.end_time = time()
         self.elapsed_ms = round(
             (self.end_time - self.start_time) * 1000, 5)
-        print(self.elapsed_ms)
+        print('{} | {} ms'.format(self.key, self.elapsed_ms), file=stderr)
 
     @staticmethod
     def __is_active():
@@ -35,4 +39,4 @@ class Benchmark():
     def print_info(message):
         if not Benchmark.__is_active():
             return
-        print(message)
+        print('- ' + message, file=stderr)
