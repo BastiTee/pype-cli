ec75b33b0b0ff1ef3cac5e16be50694205456a59
Basti Tee <basti.tee@posteo.de>
2020-12-27 22:12:07 +0100

    Extend pype pype.config system-info with venv infos

--- pype/config/system_info.py
+++ pype/config/system_info.py
@@ -8,7 +8,9 @@ from os import environ
 import click
 from tabulate import tabulate
 
+from pype.config_handler import PypeConfigHandler
 from pype.constants import ENV_CONFIG_FOLDER
+from pype.core import in_virtualenv
 from pype.util.cli import fname_to_name
 
 
@@ -17,13 +19,29 @@ def main():
     """Script's main entry point."""
     unset = 'Not set'
     print('PYPE SYSTEM ENVIRONMENT')
+    # System
     infos = [
         ['MACHINE', platform.machine()],
         ['PROCESSOR', platform.processor()],
         ['PLATFORM', platform.platform()], ['VERSION', platform.version()],
         ['RELEASE', platform.release()], ['SYSTEM', platform.system()],
-        ['PY VERSION', sys.version], ['PY VERSION_INFO', sys.version_info],
-        ['SHELL', environ.get('SHELL', unset)],
-        ['CONFIG FOLDER', environ.get(ENV_CONFIG_FOLDER, unset)]
+        ['PY VERSION', sys.version.replace('\n', '')], [
+            'PY VERSION_INFO', sys.version_info],
+        ['SHELL', environ.get('SHELL', unset)]
     ]
+    # Relevant environment infos
+    env_config_folder_set = ENV_CONFIG_FOLDER in environ.keys()
+    infos.append(['$' + ENV_CONFIG_FOLDER + ' SET', env_config_folder_set])
+    env_keys = [key for key in environ.keys() if 'pype_' in key.lower()]
+    for env_key in env_keys:
+        infos.append(
+            [env_key, environ[env_key]]
+        )
+    # Effective configuration
+    infos.append(
+        ['EFFECTIVE CONFIG FILE', PypeConfigHandler(init=True).filepath]
+    )
+    infos.append(
+        ['IN VIRTUAL ENV', in_virtualenv()]
+    )
     print(tabulate(infos))
