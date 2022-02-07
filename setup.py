1835439cecedcd2597f7917edcea47e3c9662d7c
Basti Tee <basti.tee@posteo.de>
2022-02-07 19:55:46 +0100

    Release 0.7.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ console_scripts = [f'{shell_command}=pype.__main__:main']
 setup(
     # Basic project information
     name='pype-cli',
-    version='0.6.7',
+    version='0.7.0',
     # Authorship and online reference
     author='Basti Tee',
     author_email='basti.tee@posteo.de',
